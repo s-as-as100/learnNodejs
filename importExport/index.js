14e8a8cbdb42ada9c9a185900656bc82720d61ff
s-as-as100 <s.arifsaifi@gmail.com>
2021-06-09 00:32:44 +0530

    create ur import and export module in nodejs

---
+++ importExport/index.js
@@ -0,0 +1,6 @@
+const {add,sub} = require('./open')
+ 
+ 
+
+console.log(add(5,5))
+console.log(sub(15,5))
---
+++ importExport/open.js
@@ -0,0 +1,29 @@
+
+// module how to export this module
+const add = (a,b) => {
+return a+b;
+}
+const sub = (a,b) => {
+    return a-b;
+    }
+// every individual file in nodejs is called  module js
+
+// every module has own private scope
+
+
+
+const name = "mohd";
+
+
+
+// module.exports = add;   //  for single
+
+// module.exports.add = add;  
+// module.exports.sub = sub;  
+// 
+
+
+// using object desruction
+
+
+module.exports = {add, sub}
