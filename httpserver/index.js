9348f3aa3676d41d26bbb295c4707351aaaac700
s-as-as100 <s.arifsaifi@gmail.com>
2021-06-12 00:53:44 +0530

    creating own webserver and create nodejs routing and handled http requests in nodejs

--- httpserver/index.js
+++ httpserver/index.js
@@ -12,15 +12,38 @@
 
 // 127.0.0.1  == localhost
 
-
+// module
 const http = require('http');
 
-
+// createServer
 const server = http.createServer((req, res) => {
-    res.end("hello i love nodejs ")
+    // console.log(req.url)
+
+
+      if(req.url =="/") {
+          res.end("my home page")
+      }
+    else if (req.url =="/about") {
+        res.end("my about page")
+    }
+    else if (req.url =="/contact") {
+        res.end("my contatc page")
+    }
+    else if (req.url =="/skill") {
+        res.write()  // also work
+        res.end("my skills page")
+    }
+    else {
+        // unknown url add status code 
+     res.writeHead(404 , {"Content-type" : "text/html"})
+
+        res.end(" <h1>  404 error page doesn't exits </h1>")
+
+    }
+
 })
 
-
+// listenig
 server.listen(7000, "127.0.0.1", () => {
-    console.log("listening to port 7000")
+    console.log("listening  to port 7000")
 })
