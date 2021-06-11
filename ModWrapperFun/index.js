530ba8d24c22c6aa9163f37c232f0121b6fbe625
s-as-as100 <s.arifsaifi@gmail.com>
2021-06-11 23:54:05 +0530

    install nodemon using of run automatically  and modwrapper function

---
+++ ModWrapperFun/index.js
@@ -0,0 +1,35 @@
+const x = "mohd";
+
+console.log(x)
+
+
+// in nodejs every file called module 
+
+//  inside file we have created var and func is called private scope
+
+
+// (function() {
+
+// })  // grouping operatore
+
+
+// when you run the function all data inside the grouping opertora
+// like this
+
+// (function(exports, require, module,__filename,__dirname) {
+//  // this above paramter is not global scope 
+
+//     const x = "mohd";
+
+//     console.log(x)
+
+// })   // module wrapper function or called IIFE
+
+// beacue of module wrapper function all var and function 
+// inside it is to from private scope 
+
+
+
+/// exra 
+
+ 
